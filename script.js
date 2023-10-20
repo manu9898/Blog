df12a2fdb4f8c2d93649997228ab82648797dfe1
Emmanuel Muscat <manumuscat3@gmail.com>
2023-10-20 12:29:04 +0200

    a few modifications

--- script.js
+++ script.js
@@ -1,23 +1,12 @@
-
 const menu = document.querySelector(".wrapper");
-
-
 const menuHeight = menu.clientHeight;
 
-
 window.addEventListener("scroll", () => {
-
     const scrollPosition = window.scrollY;
 
-
     if (scrollPosition > menuHeight) {
-
         menu.classList.add("scrolled");
     } else {
-
         menu.classList.remove("scrolled");
     }
 });
-
-
-
